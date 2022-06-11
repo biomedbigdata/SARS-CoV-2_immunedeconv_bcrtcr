0342acc0522c347e93c42be2acf1f35b962063b9
Lina Willruth <lina-liv.willruth@tum.de>
2022-06-11 14:49:36 +0200

    script used for nuns qc

---
+++ src/qc.R
@@ -0,0 +1,22 @@
+## script for quality control
+
+library(data.table)
+library(ggplot2)
+
+# create mapped reads per contig plot and chack if percent of mitochondrial genes is higher than 10%
+DIR <- "/nfs/data2/covid_hennighausen/vaccine_studies_nuns/03_nfcore_rnaseq_results/star_salmon/samtools_stats"
+filenames <- list.files(DIR, pattern="*.idxstats", full.names=T)
+names(filenames) <- basename(filenames)
+tables <- lapply(filenames, fread) # read all files at once into a list of data.tables
+idxstats_dt <- rbindlist(tables, idcol = 'filename')
+colnames(idxstats_dt) <- c("filename", "contig", "contig_length", "n_mapped", "n_unmapped")
+
+idxstats_dt[, percent_mapped := n_mapped/sum(n_mapped), by = filename]
+
+ggplot(idxstats_dt[contig %in% c(c(1:22), "MT", "X", "Y")], aes(x = contig, y = percent_mapped, color = filename)) +
+  geom_point() + theme(legend.position="none")
+
+# all samples with MT percent > 10 %
+unlist(lapply(idxstats_dt[contig == "MT" & percent_mapped > 0.1]$filename, function(x){
+  strsplit(x, ".", fixed=T)[[1]][[1]]
+})) 
